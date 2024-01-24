c2676bca5d5b4d90020e21030fc4f2247ea2f72e
EricGuo <2364319479@qq.com>
2024-01-24 20:44:27 +0800

    [WIP]tesseractOCR初步实现，暂时不可用

--- api_tesseractocr.py
+++ api_tesseractocr.py
@@ -4,68 +4,104 @@ import site
 import base64
 from PIL import Image
 from io import BytesIO
+import traceback
 
 # 当前目录
 CurrentDir = os.path.dirname(os.path.abspath(__file__))
 # 依赖包目录
 SitePackages = os.path.join(CurrentDir, "site-packages")
 
+TESSERACT_SUPPORTED = ['afr', 'amh', 'ara', 'asm', 'aze', 'aze', 'bel', 'ben', 'bod', 'bos', 'bre', 'bul', 'cat', 'ceb', 'ces', 'chi', 'chi', 'chr', 'cos', 'cym', 'dan', 'deu', 'div', 'dzo', 'ell', 'Mod', '145', 'eng', 'enm', 'Mid', '110', 'epo', 'equ', 'est', 'eus', 'fas', 'fao', 'fil', 'fin', 'fra', 'frk', 'frm', 'Mid', 'ca.', 'fry', 'gla', 'gle', 'glg', 'grc', 'Anc', 'to ', 'guj', 'hat', 'heb', 'hin', 'hrv', 'hun', 'hye', 'iku', 'ind', 'isl', 'ita', 'ita', 'jav', 'jpn', 'kan', 'kat', 'kat', 'kaz', 'khm', 'kir', 'kmr', 'kor', 'kor', 'lao', 'lat', 'lav', 'lit', 'ltz', 'mal', 'mar', 'mkd', 'mlt', 'mon', 'mri', 'msa', 'mya', 'nep', 'nld', 'nor', 'oci', 'ori', 'osd', 'pan', 'pol', 'por', 'pus', 'que', 'ron', 'rus', 'san', 'sin', 'slk', 'slv', 'snd', 'spa', 'spa', 'sqi', 'srp', 'srp', 'sun', 'swa', 'swe', 'syr', 'tam', 'tat', 'tel', 'tgk', 'tha', 'tir', 'ton', 'tur', 'uig', 'ukr', 'urd', 'uzb', 'uzb', 'vie', 'yid', 'yor']
+ModelDir = os.path.join(CurrentDir,"engine/tessdata/")
+
 class Api:
     def __init__(self, globalArgd):
         self.tesseractOcr = None
+    
+    def check(self,languages:list) -> (list,list):
+        unsupported = []
+        uninstalled = []
+        for language in languages:
+            if language not in TESSERACT_SUPPORTED:
+                unsupported.append(language)
+            elif not os.path.exists(ModelDir + language + ".traineddata"):
+                uninstalled.append(language)
+        return (unsupported,uninstalled)
 
-    # 获取OcrHandle 实例
-    def start(self, argd):
-        self.short_size = argd["short_size"]  # 记录最新 short_size 参数
-        self.config = {
+    def get_select_languages(self,argd) -> list:
+        selects = []
+        for k,flag in argd.items():
+            if k == "language.~enabledOther" or k == 'language.~other':
+                continue
+            if k.startswith("language.") and flag:
+                language = k[9:]
+                print(argd['vert'])
+                if (language == 'chi_sim' or language == "chi_tra") and argd['vert']:
+                        selects.append(language+"_vert")
+                selects.append(language)
+        if argd['language.~enabledOther']:
+            otherLangs = argd['language.~other'].split(" ")
+            unsupported,uninstalled = self.check(otherLangs)
+            if len(unsupported):
+                raise Exception(f"Unsupported languages: {unsupported}")
+            if len(uninstalled):
+                raise Exception(f"Uninstalled languages: {uninstalled}")
+            selects += otherLangs
+        return selects
 
-        }
-        if self.tesseractOcr:  # 引擎已启动，则跳过再启动
-            return ""
-        site.addsitedir(SitePackages)  # 依赖库到添加python搜索路径
-        try:
-            import pytesseract
-            pytesseract.pytesseract.tesseract_cmd = r'./engine/tesseract.exe'
-            self.tesseractOcr=pytesseract
-            return ""
-        except Exception as e:
-            self.tesseractOcr = None
-            err = str(e)
-            print(err)
-            return f"[Error] Error on loading:{err}"
-
-    def stop(self):
-        self.tesseractOcr = None
-
-    # 借用自plugins-P2Tocr,进行了修改
-    def _standardized(self, res):
+    def standardize(self,res):
+        # ['level', 'page_num', 'block_num', 'par_num', 'line_num', 'word_num', 'left', 'top', 'width', 'height', 'conf', 'text']
         datas = []
-        for item in res:
-            text = item[1]
-            accuracy = item[2]
-            position = item[0]
-            datas.append(
-                {
-                    "text": text,
-                    "score": accuracy,
-                    "box": position,
-                }
-            )
+        for item in res[2:]: # 第一行为固定的提示表头
+            text = item[11]
+            score = float(item[10])
+            if score == -1:
+                continue
+            top,left,width,height = int(item[6]), int(item[7]), int(item[8]), int(item[9])
+            topLeft = [left,top]
+            topRight = [left+width,top]
+            bottomLeft = [left,top+height]
+            bottomRight = [left+width,top+height]
+            box = [topLeft,topRight,bottomLeft,bottomRight]
+            # FIXIT: box目前实现存在异常，会影响box显示以及竖屏的排版
+            datas.append({"text":text,"score":score,"box":box})
         if datas:
             out = {"code": 100, "data": datas}
         else:
             out = {"code": 101, "data": ""}
         return out
 
+    # 获取OcrHandle 实例
+    def start(self, argd):
+        self.psm = "--psm 3" if argd['psm'] else "--psm 6" # psm 3: 自动分页 psm6: 单文本块分页  magic number来源：tesseract docs
+        try:
+            langs = self.get_select_languages(argd)
+            self.languages = "+".join(langs)
+            print(self.languages)
+            if self.tesseractOcr:  # 引擎已启动，则跳过再启动
+                return ""
+            site.addsitedir(SitePackages)  # 依赖库到添加python搜索路径
+            import pytesseract
+            pytesseract.pytesseract.tesseract_cmd = os.path.join(CurrentDir,'engine/tesseract.exe')
+            self.tesseractOcr=pytesseract
+            return ""
+        except Exception as e:
+            self.tesseractOcr = None
+            err = str(e)
+            return f"[Error] Error on loading engine:{err}"
+
+    def stop(self):
+        self.tesseractOcr = None
+
     def _run(self, img: Image):
         if not self.tesseractOcr:
             res = {"code": 201, "data": "tesseractOcr not initialized."}
         else:
             try:
-                res = self.tesseractOcr.image_to_data(img,**self.config)
-                res = self.mergeData(res)
-                res = self._standardized(res)
+                res = [item.split('\t') for item in self.tesseractOcr.image_to_data(img, lang=self.languages,config=self.psm).split('\n')][:-1] # TODO: 此处tesseract docs实际上给出的command line example很少，所以此处的config以最重要的psm先代替，其他的需要再多研究一下docs再加入
+                res = self.standardize(res)
             except Exception as e:
+                traceback.print_exc()
                 return {"code": 202, "data": f"tesseractOcr recognize error:{e}"}
         return res
 
--- tesseractocr_config.py
+++ tesseractocr_config.py
@@ -10,14 +10,46 @@ globalOptions = {
 localOptions = {
     "title": tr("TesseractOCR（本地）"),
     "type": "group",
-    "short_size": {
-        "title": tr("限制图像边长"),
-        "optionsList": [
-            [960, "960 " + tr("（默认）")],
-            [2880, "2880"],
-            [4320, "4320"],
-            [999999, tr("无限制")],
-        ],
-        "toolTip": tr("将边长大于该值的图片进行压缩，可以提高识别速度。可能降低识别精度。"),
+    "language": {
+        "title": "语言",
+        "type": "group",
+        "toolTip": "请在仅当文本内容包含多语言时再勾选额外识别语言，否则可能会出现识别精度下降问题。", # FIXIT: 目前该tooltip是失效状态。
+        "enabledFold": True,  # 启用折叠
+        "fold": False,  # 默认非折叠状态。（折叠状态会保存）
+        "chi_sim": {
+            "title": "中文（简体）",
+            "default": True,
+        },
+        "chi_tra": {
+            "title": "中文（繁体）",
+            "default": False,
+        },
+        "eng": {
+            "title": "English",
+            "default": False,
+        },
+        "jpn": {
+            "title": "日文",
+            "default": False,
+        },
+        "~enabledOther": {
+            "title": "启用自定义语言短码",
+            "default": False,
+        },
+        "~other": {
+            "title": "自定义语言短码",
+            "toolTip":  "请查看tesseract官方文档，使用空格对所选语言进行分割。",
+            "default": "",
+        },
     },
+    "psm":{
+        "title": "自动识别排版",
+        "toolTip": "设置分段格式为自动识别多块文本块排版格式，否则采用单文本块格式识别",
+        "default": True,
+    },
+    "vert":{
+        "title": "开启竖版识别",
+        "toolTip": "仅限中文",
+        "default": True,
+    }
 }
